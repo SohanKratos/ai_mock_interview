5bb3e0615ed949f8e8a14c39426d7ee79febab65
SohanKratos <sohankratos@gmail.com>
2025-06-14 20:03:59 +0530

    ignore eslint and typescript errors (for now)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
-import InterviewCard from '@/components/InterviewCard'
-import { Button } from '@/components/ui/button'
-import { dummyInterviews } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import InterviewCard from '@/components/InterviewCard';
+import { Button } from '@/components/ui/button';
+import { dummyInterviews } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
-const page = () => {
+const Page = () => {
   return (
     <>
       <section className="card-cta">
@@ -13,39 +13,41 @@ const page = () => {
           <h2>Get Interview-Ready With AI-Powered Practice & Feedback</h2>
           <p className="text-lg">
             Get Interview-Ready With AI-Powered Practice & Feedback
-            
           </p>
           <Button asChild className="btn-primary max-sm:w-full">
-            <Link href="/interview">Start an Interview</Link> 
+            <Link href="/interview">Start an Interview</Link>
           </Button>
         </div>
 
-        <Image src="/robot.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden" />
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
-
         <div className="interviews-section">
-          {dummyInterviews.map((interview) =>(
-            <InterviewCard {... interview} key={interview.id} />
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
           ))}
-
-          {/*<p>You Haven't Taken Any Interviews Yet</p>*/}
+          {/* <p>You Haven't Taken Any Interviews Yet</p> */}
         </div>
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take An Interview</h2>
-
         <div className="interviews-section">
-          {dummyInterviews.map((interview) =>(
-            <InterviewCard {... interview} key={interview.id} />
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
           ))}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
